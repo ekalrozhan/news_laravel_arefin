5e50a514338a9fcd7ae900da2902b4309b5c3c32
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-09-09 22:30:16 +0800

    layout, admin midlleware, login, logout

---
+++ resources/views/admin/layout/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+
+    
+    <link rel="icon" type="image/png" href="{{ asset('uploads/favicon.png') }}">
+
+    <title>Admin Panel</title>
+
+    @include('admin.layout.styles')
+    @include('admin.layout.scripts')
+    
+</head>
+
+<body>
+<div id="app">
+    <div class="main-wrapper">
+
+        @include('admin.layout.nav')
+
+
+
+      @include('admin.layout.sidebar')
+
+       @yield('main_content')
+
+    </div>
+</div>
+
+@include('admin.layout.scripts_footer')
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// admin
+Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home')->middleware('admin:admin');
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+
+Route::post('/admin/login-submit', [AdminLoginController::class, 'login_submit'])->name('admin_login_submit');
+Route::get('/admin/logout', [AdminLoginController::class, 'logout'])->name('admin_logout');
+
+Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
+
